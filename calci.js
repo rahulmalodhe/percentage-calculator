935b75a5b9c6ee7a7cc7789f283bccf86cf20cd3
rahulmalodhe <65765415+rahulmalodhe@users.noreply.github.com>
2020-10-01 18:31:42 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,17 @@
+var firstnum = document.getElementById('firstnum');
+var secondnum = document.getElementById('secondnum');
+var resultfield = document.getElementById('resultfield');
+var form = document.getElementById('xIsWhatPercentOfY');
+form.addEventListener('submit',function(event){
+    if (!firstnum.value || !secondnum.value){
+        alert("please enter values in the field")
+    }
+    else{    
+        var x = parseFloat(firstnum.value);
+        var y = parseFloat(secondnum.value);
+        var result =x/y;
+        var percent = result * 100;
+        resultfield.innerText = "Answer" + percent + "%";
+        event.preventDefault();
+    }
+});
